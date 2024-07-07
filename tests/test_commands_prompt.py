f1f918b85118f36ea197b2a57754cc58f4f42b73
vsc <nb24575@gmail.com>
2024-07-07 13:51:03 +0100

    tests : assert python version

--- tests/test_commands_prompt.py
+++ tests/test_commands_prompt.py
@@ -14,6 +14,11 @@ HighlightTypes = file_filter.HighlightTypes
 
 class TestCommandPrompt(TestCase):
 
+    @classmethod
+    def setUpClass(cls):
+        sys_version = sys.version_info.major * 10 + sys.version_info.minor # 38 '3.8'
+        self.assertGreaterEqual(sys_version, 38)
+
     def setUp(self):
         self.window = sublime.active_window()
         self.view = self.window.new_file()
